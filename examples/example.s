813ac1b401d631d0fe1044fe30ca79d5d6da1048
Severin Orth <severin.orth@st.ovgu.de>
2014-07-09 04:17:33 +0200

    Starting to do semantics of the armv4 language in kframework

---
+++ examples/example.s
@@ -0,0 +1,18 @@
+sum:
+	add	r0, r0, r1
+	bx	lr
+subtract:
+    sub r0, r0, r1
+    bx	lr
+lower:
+    subs r2, r0, r1
+    movne r0, r1
+    bx	lr
+main:
+    mov r0, #2
+    mov r1, #3
+    mov r2, r0
+    mov r0, #5
+    bl sum
+    bl subtract
+    bl lower
