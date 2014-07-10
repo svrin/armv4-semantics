4bd50a03ff37d1fb2cdde0ae62c313659f459ed2
Severin Orth <severin.orth@st.ovgu.de>
2014-07-10 15:19:52 +0200

    Change conditions of examples to not produce HOLE

--- examples/example.s
+++ examples/example.s
@@ -6,13 +6,13 @@ subtract:
     bx	lr
 lower:
     subs r2, r0, r1
-    movne r0, r1
+    moveq r0, r1
     bx	lr
 main:
     mov r0, #2
     mov r1, #3
     mov r2, r0
     mov r0, #5
-    bl sum
+    bl sumzu
     bl subtract
     bl lower
--- examples/small.s
+++ examples/small.s
@@ -2,4 +2,4 @@ main:
     mov r0, #5
     mov r1, #9
     subs r0, r0, r1
-    movmi r3, #2
+    movpl r3, #2
